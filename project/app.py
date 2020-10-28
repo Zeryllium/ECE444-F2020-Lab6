cc9e60eb6c1a14f7e89b553da50d1e4034c7491c
Zeryllium <davidcw.zhang@mail.utoronto.ca>
2020-10-27 22:51:06 -0400

    [Lab 6] Added login enforcement for post deletion

--- project/app.py
+++ project/app.py
@@ -1,10 +1,15 @@
-import sqlite3
+from functools import wraps
 from pathlib import Path
 
-from flask import Flask, g, render_template, request, session, \
-    flash, redirect, url_for, jsonify
+from flask import Flask
+from flask import flash
+from flask import jsonify
+from flask import redirect
+from flask import render_template
+from flask import request
+from flask import session
+from flask import url_for
 from flask_sqlalchemy import SQLAlchemy
-
 
 basedir = Path(__file__).resolve().parent
 
@@ -25,6 +30,16 @@ app.config.from_object(__name__)
 db = SQLAlchemy(app)
 
 from project import models
+
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not session.get('logged_in'):
+            flash('Please log in.')
+            return jsonify({'status': 0, 'message': 'Please log in.'}), 401
+        return f(*args, **kwargs)
+    return decorated_function
 
 
 @app.route('/')
@@ -71,11 +86,13 @@ def logout():
 
 
 @app.route('/delete/<int:post_id>', methods=['GET'])
+@login_required
 def delete_entry(post_id):
     """Deletes post from database."""
     result = {'status': 0, 'message': 'Error'}
     try:
-        db.session.query(models.Post).filter_by(id=post_id).delete()
+        new_id = post_id
+        db.session.query(models.Post).filter_by(id=new_id).delete()
         db.session.commit()
         result = {'status': 1, 'message': "Post Deleted"}
         flash('The entry was deleted.')
